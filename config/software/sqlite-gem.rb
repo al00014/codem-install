54df79b87ddbe4711b210277c33219d1004a08c1
Bart Zonneveld <bart@madebyhiro.com>
2014-05-28 11:40:01 +0200

    Added sqlite gem

---
+++ config/software/sqlite-gem.rb
@@ -0,0 +1,21 @@
+name 'sqlite-gem'
+default_version '1.3.9'
+
+dependency "ruby"
+dependency "rubygems"
+
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+  "LD_LIBRARY_PATH" => "#{install_dir}/embedded/lib",
+  "GEM_HOME" => "#{install_dir}/embedded/lib/ruby/gems/2.1.0",
+  "GEM_PATH" => "#{install_dir}/embedded/lib/ruby/gems/2.1.0",
+  "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
+}
+
+build do
+  gem ["install",
+       "sqlite3",
+       "-v #{version}"].join(' '), :env => env
+end
