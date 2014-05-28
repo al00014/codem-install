ef53e807d7126251bb5070a1a93f6935887e3f5e
Bart Zonneveld <bart@madebyhiro.com>
2014-05-28 11:40:45 +0200

    Updated codem-scheduler to use sqlite, and added post-install steps

--- config/software/codem-scheduler.rb
+++ config/software/codem-scheduler.rb
@@ -5,8 +5,8 @@ dependency "ruby"
 dependency "rubygems"
 dependency "bundler"
 dependency "nokogiri"
-dependency "mysql-client"
-dependency "mysql2-gem"
+dependency "sqlite"
+dependency "sqlite-gem"
 dependency "json-gem"
 dependency "rsync"
 
@@ -23,9 +23,28 @@ source :git => 'https://github.com/madebyhiro/codem-schedule.git'
 build do
   scheduler_dir = "#{install_dir}/codem-scheduler"
 
+  # Convert Gemfile to sqlite3
+  command "sed -i '' 's/mysql2/sqlite3/g' Gemfile"
+  # Convert database.yml to sqlite3
+  command "sed -i '' 's/mysql2/sqlite3/g' config/database.yml"
+  # Ensure Rails in production is serving static assets
+  command "sed -i '' 's/config.serve_static_assets = false/config.serve_static_assets = true/g' config/environments/production.rb"
+
   # Install gems without dev and test
   gem "install bundler", :env => env
-  bundle "install --binstubs --without development test", :env => env
+  bundle "install --without development test", :env => env
+
+  assets_env = {
+    "RAILS_ENV" => "production",
+    "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}"
+  }.merge!(env)
+
+  # Generate assets
+  bundle "exec rake assets:precompile", :env => assets_env
+
+  # Init database
+  bundle "exec rake db:create",  :env => assets_env
+  bundle "exec rake db:migrate", :env => assets_env
 
   # Generate destination and copy the actual code to the correct location
   command "mkdir -p #{scheduler_dir}"
@@ -42,5 +61,11 @@ build do
     :source => "bundle_exec_wrapper.erb",
     :mode => 0755,
     :vars => {:command => 'rails "$@"', :install_dir => install_dir}
+
+  # Create a start script
+  erb :dest => "#{install_dir}/run.sh",
+    :source => "run.sh.erb",
+    :mode => 0755,
+    :vars => {:install_dir => install_dir, :scheduler_dir => scheduler_dir}
 end
 
