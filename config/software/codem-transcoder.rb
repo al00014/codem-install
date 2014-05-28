17717a54f9e2eb686396536b1a97442fa4059978
Bart Zonneveld <bart@madebyhiro.com>
2014-05-28 15:33:46 +0200

    Added run script for transcoder

--- config/software/codem-transcoder.rb
+++ config/software/codem-transcoder.rb
@@ -13,20 +13,31 @@ build do
   transcoder_dir = "#{install_dir}/codem-transcoder"
 
   command "npm install"
-  
-  # Generate destination and copy the actual code to the correct location
-  command "mkdir -p #{transcoder_dir}"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{transcoder_dir}"
 
+  # Create directories needed for the provided config file
   command "mkdir #{install_dir}/log"
   command "mkdir #{install_dir}/db"
   command "mkdir #{install_dir}/config"
 
-  erb :dest => "#{install_dir}/config/config.json",
+  # Create a config file for localhost:8080
+  erb :dest => "#{install_dir}/config/localhost_8080.json",
     :source => "config.json.erb",
     :mode => 0755,
     :vars => { :log_dir => "#{install_dir}/log", 
                :db_dir => "#{install_dir}/db",
-               :bin_dir => "#{install_dir}/embedded/bin" }
+               :bin_dir => "#{install_dir}/embedded/bin",
+               :port => '8080' }
+
+  # Create a start script
+  erb :dest => "#{install_dir}/run.sh",
+    :source => "run.sh.erb",
+    :mode => 0755,
+    :vars => {:install_dir => install_dir,
+              :transcoder_dir => transcoder_dir,
+              :config_file => "config/localhost_8080.json"}
+
+  # Generate destination and copy the actual code to the correct location
+  command "mkdir -p #{transcoder_dir}"
+  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{transcoder_dir}"
 end
 
